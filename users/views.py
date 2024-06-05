dcfe479d381b4db208f262cba8756b3a5a7ea180
Пользователь <smtishkar@mail.ru>
2024-06-05 14:12:29 +0300

    реализована аутентификация пользователей (база)

---
+++ users/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
+from .forms import LoginUserForm
+
+# Create your views here.
+
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username = cd['username'], password = cd['password'])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect("/")
+    else:
+        form = LoginUserForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login'))
